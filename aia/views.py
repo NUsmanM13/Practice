0d0e8659bc074f14cc26780b2f8464fb235b3523
NUsmanM13 <niyazbekovusmon@gmail.com>
2025-10-15 15:11:06 +0500

    added home page template

--- aia/views.py
+++ aia/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def HomePageView(request):
+    ctx = {
+
+    }
+    return render(request, 'index.html', ctx)
--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+# Static fayllar URL manzili
+STATIC_URL = '/static/'
+
+# Loyihangizdagi asosiy static papkaga yo‘l
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Deploy (production) uchun:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
